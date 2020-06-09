d5f121368ef514793c820976005c9c9e35212eef
Pérez Robledo Edson Daniel <edsondanyel@gmail.com>
2020-06-09 15:37:31 -0500

    add meta tag

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
     <title>@yield('title')</title>
     
     <!-- Stylesheet -->
