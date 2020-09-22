f10dd8076ce2845a8418857d4a99b0468001fd53
Pérez Robledo Edson Daniel <edsondanyel@gmail.com>
2020-09-22 18:09:55 -0500

    fix errors css

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,6 +6,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
+
   <title>Administración | Casa Martínez</title>
 
   <!-- Font Awesome Icons -->
