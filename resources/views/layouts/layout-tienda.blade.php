03516c99d13c8faae3a295f82a73460cb2474b45
Pérez Robledo Edson Daniel <edsondanyel@gmail.com>
2020-09-23 11:37:24 -0500

    responsive shop

--- resources/views/layouts/layout-tienda.blade.php
+++ resources/views/layouts/layout-tienda.blade.php
@@ -13,7 +13,7 @@
     
     <!-- Stylesheet -->
     <!--<link href="" rel="stylesheet">-->
-    <link rel="stylesheet" href="adminlte/plugins/fontawesome-free/css/all.min.css" type="text/css">
+    <link rel="stylesheet" href="/adminlte/plugins/fontawesome-free/css/all.min.css" type="text/css">
     <link rel="stylesheet" href="{{ asset('css/custom/bootstrap.css') }}" type="text/css">
     <link href="{{ asset('css/custom/principal.css') }}" rel="stylesheet" type="text/css">    
     <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;1,400&family=Markazi+Text&family=Raleway:wght@300;400;500;600&display=swap" rel="stylesheet">    
