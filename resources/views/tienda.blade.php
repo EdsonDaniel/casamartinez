b61c4b56495656cfded848b9a797fbb16dbc13b5
Pérez Robledo Edson Daniel <edsondanyel@gmail.com>
2020-09-24 14:48:20 -0500

    bugs responsive

--- resources/views/tienda.blade.php
+++ resources/views/tienda.blade.php
@@ -456,17 +456,17 @@
 @section('more-content')
   <!--modal-->
   <div class="modal fade" id="modalProduct" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+    <div class="modal-dialog modal-dialog-centered mx-4 m-md-auto modal-lg" role="document">
       <div class="modal-content">
         <!-- Close -->
-        <button type="button" class="close btn-close-modal" data-dismiss="modal" aria-label="Close">
+        <button style="z-index: 5;" type="button" class="close btn-close-modal" data-dismiss="modal" aria-label="Close">
           <span class="fe fe-x" aria-hidden="true" >&times;</span>
         </button>
 
         <!-- Content -->
         <div class="container-fluid px-xl-0">
           <div class="row align-items-center mx-xl-0">
-            <div class="col-12 col-lg-6 col-xl-5 py-4 py-xl-0 px-xl-0">
+            <div class="col-12 col-lg-6 col-xl-5 py-3 py-md-4 py-xl-0 px-xl-0">
 
               <!-- Image -->
               <img class="img-fluid d-block my-md-5 " img-modal  src="/img/botellas/anejo-sq.jpg" alt="...">
@@ -480,10 +480,10 @@
             <div class="col-12 col-lg-6 col-xl-7 py-9 px-md-9">
 
               <!-- Heading -->
-              <h4 class="mb-2 mb-md-3 title-product-modal">Mezcal Sinái(Joven) 750ml</h4>
+              <h4 class="mb-1 mb-md-2 mb-md-3 title-product-modal">Mezcal Sinái(Joven) 750ml</h4>
 
               <!-- Price -->
-              <div class=" mb-3">
+              <div class="mb-1 mb-md-3">
                 <h4 class="price-product-modal">$850.00 MXN</h4>
                 <span class="product-status">Disponible</span>
               </div>
@@ -494,7 +494,7 @@
                 <div class="row mx-0">
 
                   <!--col-left-->
-                  <div class="col-7 px-0">
+                  <div class="col-12 col-md-7 px-0">
                     <div class="form-group">
 
                       <!-- Label -->
@@ -503,7 +503,7 @@
                       </p>
 
                       <!-- Radio -->
-                      <div class="align-content-center">
+                      <div class="div-img-radio-products">
                         <div class="custom-control custom-control-inline custom-control-img">
                           <input type="radio" class="custom-control-input" id="modalProductColorOne" name="modalProductColor" data-toggle="form-caption" data-target="#modalProductColorCaption" value="White" checked="">
                           <label class="custom-control-label pb-1" for="modalProductColorOne">
@@ -528,11 +528,11 @@
                   <!--col-left-->
 
                   <!--col-right-->
-                  <div class="col-4 px-0 d-flex justify-content-center">
-                    <div class="mt-3">
-                      <div class="form-group d-flex flex-column">
-                        <label class="text-center" for="inputCantidad">Cantidad:</label>
-                        <div class="input-group mt-1">
+                  <div class="col-12 col-md-4 px-0 d-flex justify-content-center mt-1">
+                    <div class="mt-md-3">
+                      <div class="form-group d-flex flex-md-column align-items-center">
+                        <label class="text-center mr-3 mr-md-0 my-0" for="inputCantidad">Cantidad:</label>
+                        <div class="input-group mt-md-2">
                           <div class="input-group-prepend">
                             <div class="input-group-text p-0">
                               <button type="button" class="btn icon-input-number">-</button>
@@ -550,16 +550,16 @@
                   </div>
                   <!--col-right-->
 
-                  <div class="col-12 ">
+                  <div class="col-12 px-1 px-md-auto">
                     <!-- Submit -->
-                    <button type="submit" class="btn btn-block btn-dark my-2 btn-modals p-2">
+                    <button type="submit" class="btn btn-block rounded-0 my-1 my-md-2 btn-modals p-2">
                       Agregar al carrito <i class="fe fe-shopping-cart ml-2"></i>
                     </button>
                   </div>
 
-                  <div class="col-12 ">
+                  <div class="col-12 px-1 px-md-auto">
                     <!-- Submit -->
-                    <button type="submit" class="btn btn-block btn-dark mb-2 btn-modals p-2">
+                    <button type="submit" class="btn btn-block rounded-0 mb-4 mb-md-auto btn-modals p-2">
                       Comprar ahora<i class="fe fe-shopping-cart ml-2"></i>
                     </button>
                   </div>
