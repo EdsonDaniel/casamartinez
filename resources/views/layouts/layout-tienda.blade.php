12a95cdba1ddf55cbb3afda2849a88dec7d2083d
Pérez Robledo Edson Daniel <edsondanyel@gmail.com>
2020-09-26 15:33:54 -0500

    correcciones de diseño

--- resources/views/layouts/layout-tienda.blade.php
+++ resources/views/layouts/layout-tienda.blade.php
@@ -152,67 +152,41 @@
   <div class="app-container">@yield('content')</div>
   <!--container content-->
 
-  <!--footer-->
-  <footer>
-    <div class="div-foot">
-        <div id="f2"><p>Tel. 951-323-0110</p></div>
-        <div id="f1">
-            <p class="text-center">
-                <a href="https://www.google.com/maps/place/San+Dionisio+Ocotepec,+Oax./@16.801013,-96.4114571,14z/data=!3m1!4b1!4m5!3m4!1s0x85c0b57286869a4f:0x26588c69bc264a18!8m2!3d16.7983019!4d-96.3965776">SAN DIONISIO OCOTEPEC, OAXACA<br> 16º48'N 96º24'W</a>
-            </p>
-        </div>
-        
-        <div id="f3">
-            <p class="mail"><a href="mailto:info@casamartinez.mx">info@casamartinez.mx</a></p>
-        </div>
-        <div id="f5">
-            <p class="links-ref"><a>PRIVACY POLICY</a> | <a href="">© 2016 – 2020</a> | <a href="">SITE CREDITS</a>
-            </p>
-        </div>
+  <div class="ft-shop mt-2">
+    <!--footer-->
+    <footer>
+      <div class="div-foot">
+          <div id="f2"><p>Tel. 951-323-0110</p></div>
+          <div id="f1">
+              <p class="text-center">
+                  <a href="https://www.google.com/maps/place/San+Dionisio+Ocotepec,+Oax./@16.801013,-96.4114571,14z/data=!3m1!4b1!4m5!3m4!1s0x85c0b57286869a4f:0x26588c69bc264a18!8m2!3d16.7983019!4d-96.3965776">SAN DIONISIO OCOTEPEC, OAXACA<br> 16º48'N 96º24'W</a>
+              </p>
+          </div>
+          
+          <div id="f3">
+              <p class="mail"><a href="mailto:info@casamartinez.mx">info@casamartinez.mx</a></p>
+          </div>
+          <div id="f5">
+              <p class="links-ref"><a>PRIVACY POLICY</a> | <a href="">© 2016 – 2020</a> | <a href="">SITE CREDITS</a>
+              </p>
+          </div>
 
-        <div id="f4">
-            <p class="redes icons">
-                <a href="https://twitter.com/MezcalSinai" target="_blank"><i class="fab fa-twitter"></i></a>
-                <a href="https://www.facebook.com/Mezcal.Sinai" target="_blank"><i class="fab fa-facebook-square"></i></a>
-                <a href="https://www.instagram.com/mezcal.sinai" target="_blank"><i class="fab fa-instagram"></i></a>
-            </p>
-        </div>  
-    </div>
-  </footer>
-  <!--footer-->
+          <div id="f4">
+              <p class="redes icons">
+                  <a href="https://twitter.com/MezcalSinai" target="_blank"><i class="fab fa-twitter"></i></a>
+                  <a href="https://www.facebook.com/Mezcal.Sinai" target="_blank"><i class="fab fa-facebook-square"></i></a>
+                  <a href="https://www.instagram.com/mezcal.sinai" target="_blank"><i class="fab fa-instagram"></i></a>
+              </p>
+          </div>  
+      </div>
+    </footer>
+    <!--footer-->
   </div>
 
+</div>
 
-   <!-- Scripts --
-   <script type="text/javascript">
-/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
-var dropdown = document.getElementsByClassName("boton");
-var i;
-var icon;
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-  this.classList.toggle("active");
-  var btn = this.lastElementChild;
-  icon = btn.firstElementChild;
-  icon.classList.toggle("fa-minus");
-  icon.classList.toggle("fa-plus");
-  
-  var dropdownContent = this.nextElementSibling;
-  btn = dropdownContent.firstElementChild;
-  var alt = btn.offsetHeight;
-  alt += 6;
-  alt = alt+"px";
-  /*console.log(alt);
-  console.log(dropdownContent.style.height);*/
-  if (dropdownContent.style.height === "") {
-    console.log(alt);
-  dropdownContent.style.height = alt;
-  } else {
-  dropdownContent.style.height = "";
-  }
-  });
-}
-</script>-->
+
+   
    <!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
    
    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>-->
